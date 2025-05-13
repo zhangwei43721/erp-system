d15c91204c300860cd56cc17212a0f2778627850
zhangwei43721 <zhangwei4372@outlook.com>
2025-05-13 17:16:36 +0800

    修改配置，屏蔽一些浏览器警告

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.plugin('define').tap((args) => {
+      args[0]['__VUE_OPTIONS_API__'] = true // 启用 Options API（根据需要可设为 false）
+      args[0]['__VUE_PROD_DEVTOOLS__'] = false // 生产环境禁用开发者工具
+      args[0]['__VUE_PROD_HYDRATION_MISMATCH_DETAILS__'] = false // 禁用水合不匹配详情
+      return args
+    })
+  }
 })
